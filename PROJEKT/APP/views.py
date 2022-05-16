bf6903d359caee86d2316651f41cdbddb38c15ed
Szantai Gabor <szantai.gabor.19f@szlgbp.hu>
2022-05-16 11:05:53 +0200

    segitett

--- PROJEKT/APP/views.py
+++ PROJEKT/APP/views.py
@@ -1,7 +1,7 @@
 from re import template
 from django.shortcuts import render
 
-from PROJEKT.APP.models import SajatUser
+from .models import SajatUser
 #from .models import Kerdes
 
 # Create your views here.
@@ -11,11 +11,15 @@ def index(request):
 
     if request.method=="POST":
         idje = request.POST['diak_azonos']
+        #print(request.POST['nev'])
 
         lista = list(SajatUser.objects.filter(azonosito=idje))
+        print(lista)
+        talalat = lista[0]
+        
         #Kerdes.objects.create(kerdes=kerdesnev)
 
 
     template= "index.html"
-    context={}#{'kerdesek': Kerdes.objects.all()}
+    context={'userneve': lista[0].nev, 'userpont':lista[0].pontszam}
     return render(request,template, context)
